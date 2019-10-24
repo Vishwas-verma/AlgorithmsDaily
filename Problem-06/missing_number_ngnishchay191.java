0d5e9fa9bf7d7f66288c633b3ff70cf84688e168
Nishchay Gupta <ngnishchay191@gmail.com>
2019-10-24 18:40:51 +0530

    adding Problem-06/missing_number_ngnishchay191.java

---
+++ Problem-06/missing_number_ngnishchay191.java
@@ -0,0 +1,35 @@
+/*
+
+Author : ngnishchay191 (Nishchay Gupta)
+College : Maharaja Agrasen Institute Of Technology, New Delhi
+Year/Department : 2nd Year/CSE
+E-Mail Id : ngnishchay191@gmail.com
+
+*/
+import java.util.*;
+
+public class missing_number_ngnishchay191 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		int sum = 0;
+		
+		for(int i = 0; i < n; i++) {
+			int temp = scan.nextInt();
+			sum = sum + temp;
+		}
+		
+		int sumOfN = ((n+1) * n )/2;
+		
+		int missing = sumOfN - sum;
+		
+        System.out.println(missing);
+        
+        scan.close();		
+
+	}
+
+}
